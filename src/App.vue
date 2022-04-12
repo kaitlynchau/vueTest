ce86ed1d9e4c3cef963d135dc8b7409608e91836
Kaitlyn Chau <kaitlynchau@Kaitlyns-MacBook-Pro-2.local>
2022-04-11 22:58:53 -0400

    vue test

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import Status from './components/Status.vue';
+
+export default {
+  components: {
+    Status
+  },
+  data() {
+    return {
+      discord_status: null
+    };
+  },
+  async created() {
+    const response = await fetch("https://api.lanyard.rest/v1/users/721863579787329578");
+    const data = await response.json();
+    console.log(data);
+    this.discord_status = data.data.discord_status;
+  }
+}
+</script>
+
+<template>
+  <div class="w-screen h-screen bg-orange-100">
+    <div class="flex flex-col h-full justify-center items sm:w-full md:max-w-xl m-auto">
+      <div class="bg-orange-50 p-8 rounded-lg h-full w-full md:h-auto md:w-auto">
+        <h1 class="text-3xl">
+          Kaitlyn Chau
+        </h1>
+        <Status :discord_status="discord_status" />
+        <p class="py-2" />
+        <p>hi there!</p>
+        <p>
+          i am a freshman at NYU studying computer science. my website is still a work in progress, but I hope we can keep in touch:
+        </p>
+        <p class="py-2" />
+        <a class="text-blue-500 hover:text-blue-300 transition" href="mailto:kaitlyn.chau@nyu.edu">
+          email me here!
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
---
+++ src/components/Status.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    discord_status: String
+  }
+}
+</script>
+
+<template>
+  <div class="flex items-center">
+    <div v-if="discord_status === 'idle'" class="flex w-4 h-4 ">
+      <div class="relative inline-flex w-4 h-4 bg-yellow-400 rounded-full"/>
+    </div>
+    <div v-else-if="discord_status === 'dnd'" class="flex w-4 h-4 ">
+      <div class="relative inline-flex w-4 h-4 bg-red-400 rounded-full"/>
+    </div>
+    <div v-else-if="discord_status === 'offline'" class="flex w-4 h-4 ">
+      <div class="relative inline-flex w-4 h-4 bg-neutral-400 rounded-full"/>
+    </div>
+    <div v-else class="flex w-4 h-4 ">
+      <div class="animate-ping absolute inline-flex w-4 h-4 bg-green-400 rounded-full"/>
+      <div class="relative inline-flex w-4 h-4 bg-green-400 rounded-full"/>
+    </div>
+    <div class="px-1" />
+    <p class="uppercase">
+      {{discord_status}}
+    </p>
+  </div>
+</template>
